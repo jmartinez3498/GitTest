abbc1d9fd73bdc3d79cfcf24e92109d9856f0ab9
Jonathan Martinez <jmartinez9029@gmail.com>
2020-05-26 14:00:29 -0700

    created a vector to store user strings inputs

--- GitProjectTest/GitTest.cpp
+++ GitProjectTest/GitTest.cpp
@@ -4,18 +4,28 @@
 
 int main()
 {
-/* creating a longest common string counter.
-	First create something that can take input from user
-*/
+	/* creating a longest common string counter.
+		First create something that can take input from user
+	*/
 	std::vector<std::string> s;
 	std::string v;
 	std::cout << "I am a longest common string counter" << std::endl;
-	std::cout << "Enter any strings" << std::endl;
-	std::getline(std::cin, v);
-
-	for (int i = 0; i < v.length(); i++)
+	bool result = 1;
+	while (result == 1)
 	{
+		std::cout << "Enter any string. Type 0 when done." << std::endl;
+		std::cin >> v;
+		if (v=="0")
+			result = 0;
+		else if (result == 1)
+		{
+			s.push_back(v);
+		}
+	} 
 
+	for (int i = 0; i < s.size(); ++i)
+	{
+		std::cout << s[i] << std::endl;
 	}
 	
 }
