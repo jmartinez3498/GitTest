0795d80aeebff52b5ad96f7613f43be8d2d463b3
Jonathan Martinez <jmartinez9029@gmail.com>
2020-05-26 13:45:06 -0700

    Tried to store strings from getline into a vector. Realized I could just create a for loop for whatever the user entered and enter that into a vector.

--- GitProjectTest/GitTest.cpp
+++ GitProjectTest/GitTest.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <string>
 
+
+
+
 int main()
 {
 /* creating a longest common string counter.
@@ -13,8 +16,22 @@ int main()
 	std::cout << "Enter any strings" << std::endl;
 	std::getline(std::cin, v);
 
+	//testing how to get the user's inputs from getline into a vector with a for loop
+	//maybe there is an easier way if I google it. RN just testing for space seperated words
+	std::cout << v[1];
+	
+	//having problem with arguments, try to make it function
+	//didn't work, had to change arguments to be by reference
 	for (int i = 0; i < v.length(); i++)
 	{
+		int j = 0;
+		char sp = ' ';
+		int result = strcmp(&v[i], &sp);
+		if (result == 1)
+		{
+			s.push_back(v.substr(j, i));
+			j = i;
+		}
 
 	}
 	
