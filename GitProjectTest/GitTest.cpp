b7c3e39070462095b39f22cb17d40c8ddbe4b217
Jonathan Martinez <jmartinez9029@gmail.com>
2020-05-27 15:15:26 -0700

    Played with comming vs staging changes

--- GitProjectTest/GitTest.cpp
+++ GitProjectTest/GitTest.cpp
@@ -4,16 +4,16 @@
 
 int main()
 {
-	/* creating a longest common string counter.
+	/* creating a longest common word counter.
 		First create something that can take input from user
 	*/
 	std::vector<std::string> s;
 	std::string v;
-	std::cout << "I am a longest common string counter" << std::endl;
+	std::cout << "I am a longest common word counter" << std::endl;
 	bool result = 1;
 	while (result == 1)
 	{
-		std::cout << "Enter any string. Type 0 when done." << std::endl;
+		std::cout << "Enter any word. Type 0 when done." << std::endl;
 		std::cin >> v;
 		if (v=="0")
 			result = 0;
@@ -22,6 +22,11 @@ int main()
 			s.push_back(v);
 		}
 	} 
+
+	//weird nothing changed
+	//lets try again
+	//maybe it's because I didn't commit before I changed brances
+	//Actually its because I tried stashing the changes
 
 	for (int i = 0; i < s.size(); ++i)
 	{
